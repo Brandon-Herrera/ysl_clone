a687337a6082ea5d97c0c643acd954a68d36b0ad
Brandon Herrera <brandon.herrera.work@gmail.com>
2022-03-24 13:19:48 -0400

    Update LoadDB class to retrieve categories from level provided in method parameter by searching database and filtering for documents with level parameter matching the level provided in method parameter.

--- classes/app_classes.js
+++ classes/app_classes.js
@@ -5,25 +5,9 @@ class LoadDB {
 
     }
 
-    async _findLevelCategories(level) {
-        const categories = await Category.find({})
-        return categories.filter(cat => cat['ancestors'].length === level)
-    }
-
-    get firstLevelCategories() {
-        return this._findLevelCategories(1)
-    }
-
-    get secondLevelCategories() {
-        return this._findLevelCategories(2)
-    }
-
-    get thirdLevelCategories() {
-        return this._findLevelCategories(3)
-    }
-
-    get fourthLevelCategories() {
-        return this._findLevelCategories(4)
+    async findLevelCategories(level) {
+        const categories = await Category.find({level: level})
+        return categories
     }
 }
 
